204db5e6251d6cab99deb129e0c4e66603ffeee9
Kyle Bonar <kylewb2011@gmail.com>
2017-10-04 14:48:39 -0500

    Init commit. Template for all database solutions to follow.

---
+++ template.sql
@@ -0,0 +1,10 @@
+-- Author: Kyle Bonar
+-- Github: github.com/KyleBonar
+
+-- Description:
+-- Multiple lines if needed
+
+-- Example:
+-- Tables here
+
+-- --------------------------------------------------------
