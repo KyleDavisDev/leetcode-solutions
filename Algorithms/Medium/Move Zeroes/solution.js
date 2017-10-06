bff2d548d9226db8b52d21a63e67d982eec1fa04
Kyle Bonar <kylewb2011@gmail.com>
2017-10-06 10:14:10 -0500

    Added comments.

--- Algorithms/Medium/Move Zeroes/solution.js
+++ Algorithms/Medium/Move Zeroes/solution.js
@@ -17,15 +17,21 @@
  */
 
 var moveZeroes = function(nums) {
+  //basic nums checking to be sure function was passed legit data
   if (nums === null || nums.length === 0) {
     return;
   }
 
+  //loop through nums
   let count = 0;
   for (let i = 0, len = nums.length; i < len; i++) {
+    //incriment count every time a zero is found
     if (nums[i] === 0) {
       count++;
     } else {
+      //swap non-zero num w/ the first zero in chain before it
+      //In example below: the two numbers in quotes are swapped
+      //[1,'0',0,'1',0] --> [1,'1',0,'0',0] @ i = 3 && count = 2
       let temp = nums[i];
       nums[i] = nums[i - count];
       nums[i - count] = temp;
