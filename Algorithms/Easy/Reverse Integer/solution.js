9ce77dda2d617063b44873d2f6c63a8cfe47db14
Kyle Bonar <kylewb2011@gmail.com>
2017-10-19 14:05:17 -0500

    Added comments.

--- Algorithms/Easy/Reverse Integer/solution.js
+++ Algorithms/Easy/Reverse Integer/solution.js
@@ -16,13 +16,24 @@
  * @return {number}
  */
 var reverse = function(x) {
+  //check if 0 immediately so we can skip algorithm
   if (x === 0) {
     return 0;
   }
+
+  //going to construct num digit by digit from x
+  //each loop
   let num = 0;
   while (x !== 0) {
+    //move digits in num over a place and add most-right digit from x onto it.
     num = num * 10 + x % 10;
+
+    //reduce x by 10, effectively dropping most-right digit.
+    //parseInt to handle negative digits. EX: parseInt(-123/10) === parseInt(-12.3) === -12
+    //if we tried Math.floor instead, Math.floor(-123/10) === Math.floor(-12.3) === -13
     x = parseInt(x / 10);
+
+    //check if reversed integer would overflow or not.
     if (num > 2147483648 || num < -2147483648) {
       return 0;
     }
