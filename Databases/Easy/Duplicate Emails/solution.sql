d64e2e3702fc4db41dbf813c91a2f4b7807ea58e
Kyle Bonar <kylewb2011@gmail.com>
2017-10-24 12:54:41 -0500

    Init commit.

---
+++ Databases/Easy/Duplicate Emails/solution.sql
@@ -0,0 +1,34 @@
+-- Author: Kyle Bonar
+-- Github: github.com/KyleBonar
+
+-- Description:
+-- Write a SQL query to find all duplicate emails in a table named Person.
+
+-- Example:
+-- Table: Person
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+
+-- For example, your query should return the following for the above table:
+
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+
+-- --------------------------------------------------------
+
+SELECT
+    Email
+FROM
+    Person
+GROUP BY
+    Email
+HAVING
+    count(Email)>1
