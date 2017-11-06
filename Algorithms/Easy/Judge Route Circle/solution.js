25010099f1b2ac567388a436dd5cb0aa6eeb6407
Kyle Bonar <kylewb2011@gmail.com>
2017-11-06 15:52:14 -0600

    Added comments.

--- Algorithms/Easy/Judge Route Circle/solution.js
+++ Algorithms/Easy/Judge Route Circle/solution.js
@@ -18,26 +18,40 @@
 
 // --------------------------------------------------------
 
+//We are going to set a variable for horizontal movement, and a variable for
+//vertical movement. When the character "U" appears, we will increase the value
+//of vert by one. When the character "D" appears, we will decrease the value of
+//vert by one. We will do a similar process for the "L" and "R" character w/
+//repsect to the horz variable.
+// At the end, we will compare the two values along with 0.
 /**
  * @param {string} moves
  * @return {boolean}
  */
 var judgeCircle = function(moves) {
+  //init variables
   let vert = 0;
   let horz = 0;
 
+  //split string into array
   moves = moves.split("");
+  //loop through array
   for (let i = 0, len = moves.length; i < len; i++) {
     if (moves[i] === "U") {
+      //check if up
       vert++;
     } else if (moves[i] === "D") {
+      //check down
       vert--;
     } else if (moves[i] === "R") {
+      //check right
       horz++;
     } else if (moves[i] === "L") {
+      //check left
       horz--;
     }
   }
 
+  //compare and return bool
   return vert === horz && horz === 0;
 };
