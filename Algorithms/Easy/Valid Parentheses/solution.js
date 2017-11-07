de0661ba4b4d2015e15484be80f72a3b76207983
Kyle Bonar <kylewb2011@gmail.com>
2017-11-06 21:25:11 -0600

    Added comments.

--- Algorithms/Easy/Valid Parentheses/solution.js
+++ Algorithms/Easy/Valid Parentheses/solution.js
@@ -10,23 +10,41 @@
 
 // --------------------------------------------------------
 
+//We are going to create a stack and always compare the top item to the 
+//next bit of the string. If the two can form to create a match, we will
+//pop the top item off and go to next string element. At end of day, 
+//check stack size length to see if all matches were formed.
+//We can also cut some of the algo off ahead of time if the string 
+//element is a closing character that doesn't have a match. I.E. "(})}" 
+//will return false in the second loop iteration because, no matter what
+//is after the '}', we will be unable to find it a match and thus do not
+//have to go through the rest of the algo.
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
+  //check easy cases first
   if(s.length === 0 || s.length === 1) { return false; }
   
+  //split string to array and create stack
   s = s.split('');
   const stack = [];
   
+  //loop over string
   for(let i = 0, len = s.length; i < len; i++){
-      let last = stack.length-1 >= 0 ? stack[stack.length-1] + s[i] : s[i];
-      if(last === "{}" || last === "[]" || last === "()"){
-          stack.pop();
-      } else {
-          stack.push(s[i]);
-      }
+    //concat top stack element with incoming bracket
+    let last = stack.length-1 >= 0 ? stack[stack.length-1] + s[i] : s[i];
+    //check if there is a combination made
+    if(last === "{}" || last === "[]" || last === "()"){
+        stack.pop();
+    } else if(s[i] === "}" || s[i] === "]" || s[i] === ")") {
+        //return false if it is a closing
+        return false;
+    } else {
+        //else push to stack
+        stack.push(s[i]);
+    }
       
   }
 
