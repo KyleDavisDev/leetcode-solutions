de68e7f2cb675237357b4c07893fb8cccb4f9794
Kyle Bonar <kylewb2011@gmail.com>
2017-11-22 11:35:32 -0600

    Added comments.

--- Algorithms/Easy/Number Compliment/solution.js
+++ Algorithms/Easy/Number Compliment/solution.js
@@ -31,6 +31,13 @@
  * @return {number}
  */
 var findComplement = function(num) {
+  //In following commments, num will be 5
+  //1. convert num to binary via toString(2). I.e. 101
+  //2. get length of string so we can know how many bits we need. I.e. 101.length => 3
+  //3. want to convert all 3 of those bits to 1's so we raise 2 to the power of 3 which is 8. 8 in binary is 1000
+  //4. by subtracting 1 from 8, the variable 'compare' goes from binary 1000 to 111.
+  //5. compare 101 with 111 via XOR which tells us explicity which bits are turned on in ONLY one of the two numbers.
+  //6. in this case, 101 ^ 111 results in binary 10 which is 2 in base 10.
   const compare = 2 ** num.toString(2).length - 1;
   return num ^ compare;
 };
