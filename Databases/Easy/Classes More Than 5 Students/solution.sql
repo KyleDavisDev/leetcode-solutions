7c693e6ced5c9c5b18ab054f7861f77d41ee3209
Kyle Bonar <kylewb2011@gmail.com>
2017-12-06 13:52:51 -0600

    Init commit.

---
+++ Databases/Easy/Classes More Than 5 Students/solution.sql
@@ -0,0 +1,42 @@
+-- Author: Kyle Bonar
+-- Github: github.com/KyleBonar
+
+-- Description:
+-- There is a table courses with columns: student and class
+-- Please list out all classes which have more than or equal to 5 students.
+
+-- Example:
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+
+-- Result:
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+
+-- Note:
+-- The students should not be counted duplicate in each course.
+
+-- --------------------------------------------------------
+
+SELECT 
+    class 
+FROM 
+    courses
+GROUP BY
+    class 
+HAVING
+    COUNT(DISTINCT student) > 4;
