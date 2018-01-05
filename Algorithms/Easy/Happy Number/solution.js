d7e079269ec21bc019f952faace674066b18815f
Kyle Bonar <kylewb2011@gmail.com>
2018-01-05 08:15:24 -0600

    Added comments.

--- Algorithms/Easy/Happy Number/solution.js
+++ Algorithms/Easy/Happy Number/solution.js
@@ -17,18 +17,28 @@
  * @param {number} n
  * @return {boolean}
  */
+//General solution: Find the sum of the squared individual numbers, add that number to set, rinse/repeat.
+//We can stop if that number is 1 or found already in the set.
 const isHappy = n => {
+  //set will hold all of the sums we compute
   const set = new Set();
+  //add our starting number to begin with
   set.add(n);
 
+  //loop over n
   while (n !== 1) {
+    //get sum of squares
     n = squareSum(n);
 
+    //add to set or return false
     if (set.has(n)) return false;
     else set.add(n);
   }
   return true;
 };
+
+//this is set of squares so that we do not need to computer the square of a number ever single time
+//having the results pre-saved should save us computation time
 const dict = {
   1: 1,
   2: 4,
@@ -45,8 +55,13 @@ const squareSum = n => {
   let sum = 0,
     digit;
   while (n > 0) {
+    //digit will be assigned right-most value in n.
     digit = n % 10;
+    //add the squared digit value to sum
     sum = sum + dict[digit];
+    //remove the right-most digit from n and shift all other digits one space
+    //Ex if n is 154:  n = (154 - 4) / 10
+    //i.e. n = 15
     n = (n - digit) / 10;
   }
   return sum;
