25b912435dd96449a4a80bcdaeb2e711cae86328
Kyle Bonar <kylewb2011@gmail.com>
2018-01-12 11:50:30 -0600

    Added algo.

---
+++ Algorithms/Easy/Maximum Depth of Binary Tree/solution.js
@@ -0,0 +1,36 @@
+// Author: Kyle Bonar
+// Github: github.com/KyleBonar
+
+// Description:
+// Given a binary tree, find its maximum depth.
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// --------------------------------------------------------
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+
+  let maxDepth = 0;
+
+  (function DFS(node, index) {
+    if (node === null) return null;
+    else {
+      maxDepth = maxDepth > index ? maxDepth : index;
+      DFS(node.left, index + 1);
+      DFS(node.right, index + 1);
+    }
+  })(root, 1);
+
+  return maxDepth;
+};
