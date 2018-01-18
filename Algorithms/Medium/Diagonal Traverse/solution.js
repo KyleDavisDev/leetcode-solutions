97d2e684cf2710e18a178abeb4558a683aec7c06
Kyle Bonar <kylewb2011@gmail.com>
2018-01-18 15:57:14 -0600

    Added comments.

--- Algorithms/Medium/Diagonal Traverse/solution.js
+++ Algorithms/Medium/Diagonal Traverse/solution.js
@@ -22,23 +22,29 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function (matrix) {
+  //seee if we even need to go through algo or not.
   if (matrix.length === 0) return [];
+  //keep track of current row and column
   let r = c = 0;
+  //get the lengths of the input matrix and init our results array
   const m = matrix.length,
     n = matrix[0].length,
     result = [];
 
+  //loop through the total number of matrix elements (m*n)
   for (let i = 0, len = m * n; i < len; i++) {
+    //push value to matrix
     result.push(matrix[r][c]);
 
-    if ((r + c) % 2 === 0) {
-      if (c === n - 1) { r++; }
-      else if (r === 0) { c++; }
-      else { r--; c++; }
-    } else {
-      if (r === m - 1) { c++; }
-      else if (c === 0) { r++; }
-      else { r++; c--; }
+    //interesting property of this traversal question is that when we are going "up", the current r and c are evenly divisible by 2.
+    if ((r + c) % 2 === 0) { //going up
+      if (c === n - 1) { r++; } //right edge + corner (if exists)
+      else if (r === 0) { c++; } //top edge
+      else { r--; c++; } //keep going
+    } else { //going down
+      if (r === m - 1) { c++; } //bottom edge + coern (if exists)
+      else if (c === 0) { r++; } //left edge
+      else { r++; c--; } //keep going
     }
   }
 
