de878b9b70b0338cedfbc1b7a7c322cfad9c4aa5
Kyle Bonar <kylewb2011@gmail.com>
2018-01-24 14:40:21 -0600

    Init commit.

---
+++ Algorithms/Medium/Shuffle an Array/solution.js
@@ -0,0 +1,67 @@
+// Author: Kyle Bonar
+// Github: github.com/KyleBonar
+
+// Description:
+// Shuffle a set of numbers without duplicates.
+
+// Example:
+// Init an array with set 1, 2, and 3.
+// int[] nums = {1,2,3};
+// Solution solution = new Solution(nums);
+
+// // Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+// solution.shuffle();
+
+// // Resets the array back to its original configuration [1,2,3].
+// solution.reset();
+
+// // Returns the random shuffling of array [1,2,3].
+// solution.shuffle();
+
+// --------------------------------------------------------
+
+/**
+ * @param {number[]} nums
+ */
+var Solution = function (nums) {
+  this.nums = nums || [];
+};
+
+/**
+* Resets the array to its original configuration and return it.
+* @return {number[]}
+*/
+Solution.prototype.reset = function () {
+  return this.nums;
+};
+
+/**
+* Returns a random shuffling of the array.
+* @return {number[]}
+*/
+Solution.prototype.shuffle = function () {
+  if (this.nums.length === 0) return this.nums;
+
+  const arr = this.nums.slice();
+  var m = arr.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m > 0) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = arr[m];
+    arr[m] = arr[i];
+    arr[i] = t;
+  }
+
+  return arr;
+}
+
+/** 
+* Your Solution object will be instantiated and called as such:
+* var obj = Object.create(Solution).createNew(nums)
+* var param_1 = obj.reset()
+* var param_2 = obj.shuffle()
+*/
