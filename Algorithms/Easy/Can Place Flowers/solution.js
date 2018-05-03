7035f631c7eb9d11f7552a8459cdf0f1b6f90a72
Kyle Bonar <kylewb2011@gmail.com>
2018-05-03 10:52:06 -0500

    Added comments. Carried over additional code.

--- Algorithms/Easy/Can Place Flowers/solution.js
+++ Algorithms/Easy/Can Place Flowers/solution.js
@@ -21,20 +21,27 @@
 // --------------------------------------------------------
 
 /**
+ * @description Loop through flowerbed array and check for conditions which would allow a flower to be planted, incriment counter along the way, compare counter to initial condition at the end
  * @param {number[]} flowerbed
  * @param {number} n
  * @return {boolean}
  */
 const canPlaceFlowers = (flowerbed, n) => {
+  // Guard clause. If n = 0, we are done.
   if (n === 0) return true;
 
+  // Grab length once, we may need to use later
   const len = flowerbed.length;
+
+  // Guard clause. If the flowerpot has only one spot, and n > 0 (which we know is the case if we are here), we can quickly check if the spot is filled or not and return bool
   if (len === 1) {
     return flowerbed[0] === 0;
   }
 
+  // Initialize our comparison variable
   let count = 0;
 
+  // Loop through flowerbed. If we find a spot, be sure to actually 'plant' a flower for future loops to be able to refer to
   for (let i = 0; i < len; i++) {
     //edge case left
     if (i === 0 && flowerbed[i] === 0 && flowerbed[i + 1] === 0) {
@@ -44,11 +51,14 @@ const canPlaceFlowers = (flowerbed, n) => {
       flowerbed[i] = 1;
       count++;
     } else if (i === len - 1 && flowerbed[i] === 0 && flowerbed[i - 1] === 0) { //edge case right
-      flowerbed[i] = 1;
+      flowerbed[i] = 1; // Could remove this since we are already at the end of our loop but it looks good for consistancy.
       count++;
     }
 
+    //check to see if we can leave early
+    if (count === n) break;
   }
 
-  return count >= n;
+  // Since we are possibly breaking the loop early, we can confidently compare whether the two values are exactly equal or not.
+  return count === n;
 };
