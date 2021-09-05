a0b4fd5a507e364821fd831a527bd592c0d16af9
kyledavisdev <kyledavisdev@gmail.com>
2021-09-04 18:45:52 -0700

    init commit

---
+++ Algorithms/Easy/Majority Element/solution.cs
@@ -0,0 +1,34 @@
+﻿// Author: Kyle Davis
+// Github: github.com/KyleDavisDev
+
+// Description:
+// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+// You may assume that the array is non-empty and the majority element always exist in the array.
+
+// --------------------------------------------------------
+
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int counter = 0;
+        int mostFrequentVal = nums[0];
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (mostFrequentVal == nums[i])
+            {
+                counter++;
+            }
+            else if (counter > 0)
+            {
+                counter--;
+            }
+            else
+            {
+                mostFrequentVal = nums[i];
+                counter++;
+            }
+        }
+
+        return mostFrequentVal;
+    }
+}
