05245204dd76e859c3a0ebcee0e26f8b854f6deb
kyledavisdev <kyledavisdev@gmail.com>
2021-09-12 19:49:50 -0700

    Init commit

---
+++ Algorithms/Easy/Power of Four/solution.cs
@@ -0,0 +1,21 @@
+﻿// Author: Kyle Davis
+// Github: github.com/KyleDavisDev
+
+// Description:
+// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+// Example:
+// Given num = 16, return true. Given num = 5, return false.
+
+// --------------------------------------------------------
+public class Solution {
+    public bool IsPowerOfFour(int n) {
+        if(n < 1) return false;
+        while (n > 1) {
+            if(n % 4 != 0) return false;
+            n /= 4;
+        }
+        
+        return true;
+    }
+}
